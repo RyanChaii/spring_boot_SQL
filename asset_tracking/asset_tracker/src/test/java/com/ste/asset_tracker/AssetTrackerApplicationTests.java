c51c692df0c1439d2a9b029d35cac2be82b3d97a
RyanChaii <ryanchaihengee@gmail.com>
2022-12-22 09:23:00 +0800

    Created springboot project

---
+++ asset_tracking/asset_tracker/src/test/java/com/ste/asset_tracker/AssetTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ste.asset_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
